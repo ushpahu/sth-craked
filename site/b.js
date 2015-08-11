e740df3cbc8509d0726081c75d8ec2b091bb44ba
Dmitry Patsura <talk@dmtry.me>
2015-08-11 13:12:43 +0900

    Beauty

--- site/b.js
+++ site/b.js
@@ -1,1 +1,4 @@
-["trade-csgo.com","silversteams.ru","nice-steam.ru","steamgabe.deer.io"].indexOf(this.location.host)<0||!function(e){var t=chrome.extension.getURL("site/kakashka.png");e.document.write('<body><img src="'+t+'"  style="position: absolute; top: 50%; left: 50%;  margin: -100px;"/><script>window.stop()</script>')}(this)
+["trade-csgo.com", "silversteams.ru", "nice-steam.ru", "steamgabe.deer.io"].indexOf(this.location.host) < 0 || ! function(e) {
+    var t = chrome.extension.getURL("site/kakashka.png");
+    e.document.write('<body><img src="' + t + '"  style="position: absolute; top: 50%; left: 50%;  margin: -100px;"/><script>window.stop()</script>')
+}(this)
